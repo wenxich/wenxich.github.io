32f99688c29cc68747f5c21e1ea0c078c5824827
wenxich <wenxi.c.chen@gmail.com>
2023-07-17 01:16:51 -0400

    completely overthrew the ui again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // accordion
 
-var acc = document.getElementsByClassName("accordion");
+/** var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
@@ -13,7 +13,7 @@ for (i = 0; i < acc.length; i++) {
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
-}
+} **/
 
 // timezone
 const timeDiv = document.getElementById("timezone");
@@ -25,3 +25,47 @@ function setTime() {
 setInterval(function() {
     setTime();
 }, 1000);
+
+// filtering
+filterSelection("all")
+function filterSelection(c) {
+    let x, i;
+    x = document.getElementsByClassName("project-wrapper");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function w3AddClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+}
+
+function w3RemoveClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+let btnContainer = document.getElementById("filters-wrapper");
+let btns = btnContainer.getElementsByClassName("filter");
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
